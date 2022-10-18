7059d9adb5c1a2f514f30bdec3d85cdaba209657
Demet Çalışkan <demet@aposto.com>
2022-10-18 23:53:27 +0300

    Complete basic structure of the project code

---
+++ Bullseye/Views/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Demet Çalışkan on 18.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var alertIsVisible = false
+    @State private var sliderValue = 50.0
+    @State private var game = Game()
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(5.0)
+                .font(.footnote)
+            Text(String(game.target))
+                .kerning(-1.0)
+                .fontWeight(.black)
+                .font(.largeTitle)
+            HStack {
+                Text("1")
+                    .bold()
+                    .font(.body)
+                Slider(value: $sliderValue, in: 1.0...100.0)
+                Text("100")
+                    .bold()
+                    .font(.body)
+            }
+            Button(action: {
+                alertIsVisible = true
+            }) {
+                Text("HIT ME")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .kerning(1.0)
+            }
+            .alert("Hello there!", isPresented: $alertIsVisible) {
+                Button("Awesome!") {}
+            } message: {
+                let roundedValue = Int(sliderValue.rounded())
+                Text("The slider's value is: \(roundedValue).\n" +
+                     "You scored \(game.points(sliderValue: (roundedValue))) points this round.")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+    }
+}
